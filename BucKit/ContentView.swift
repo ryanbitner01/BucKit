7a516edb307b25e7607e0ee16e5bff955ae7578e
SoggyDirector <85690624+SoggyDirector@users.noreply.github.com>
2021-08-23 10:09:46 -0600

    hello

--- BucKit/ContentView.swift
+++ BucKit/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, ryan!")
             .padding()
     }
 }
@@ -18,4 +18,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    
+    
 }
