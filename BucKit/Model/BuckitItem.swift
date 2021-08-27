04cf6f68e27d40322d8bf2f3dc0ddc95bac6aef6
ryanbitner01 <79929655+ryanbitner01@users.noreply.github.com>
2021-08-27 08:05:01 -0600

    Create Buckit Item Model

---
+++ BucKit/Model/BuckitItem.swift
@@ -0,0 +1,25 @@
+//
+//  BuckitItem.swift
+//  BucKit
+//
+//  Created by Ryan Bitner on 8/27/21.
+//
+
+import Foundation
+import MapKit
+
+struct BuckitItem {
+    var name: String
+    let id: UUID = UUID()
+    let location: MKMapItem?
+    let date: Date
+    let activities: [String]
+    
+    init(name: String, location: MKMapItem?, date: Date, activities: [String] = []) {
+        self.name = name
+        self.location = location
+        self.date = date
+        self.activities = activities
+    }
+    
+}
