f8e13c870b8f521f8edf03b24ab5aa8173adb28d
ryanbitner01 <79929655+ryanbitner01@users.noreply.github.com>
2021-08-27 08:25:53 -0600

    test

--- BucKit/App/BucKitApp.swift
+++ BucKit/App/BucKitApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct BucKitApp: App {
     let persistenceController = PersistenceController.shared
-
+// This is a comment
     var body: some Scene {
         WindowGroup {
             MainTabs()
