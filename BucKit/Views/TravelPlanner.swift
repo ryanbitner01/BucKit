093f335b2b1ce8587eb2323478895d09d5b4267f
ryanbitner01 <79929655+ryanbitner01@users.noreply.github.com>
2021-09-13 10:24:16 -0600

    Started Travel Planner

---
+++ BucKit/Views/TravelPlanner.swift
@@ -0,0 +1,21 @@
+//
+//  TravelPlanner.swift
+//  BucKit
+//
+//  Created by Ryan Bitner on 9/13/21.
+//
+
+import SwiftUI
+
+struct TravelPlanner: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TravelPlanner_Previews: PreviewProvider {
+    static var previews: some View {
+        TravelPlanner()
+    }
+}
