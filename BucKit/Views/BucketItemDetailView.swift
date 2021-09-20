f67f6b13725ae7e0c191c68351ab768e37547308
Caleb Greer <caleb.greer2552@stu.mtec.edu>
2021-09-20 09:27:55 -0600

    Fixed Edit View

--- BucKit/Views/BucketItemDetailView.swift
+++ BucKit/Views/BucketItemDetailView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 import MapKit
 
-struct BucketItemDetailView: View {
-    
+struct BuckitItemViewWithNavBar: View {
+    @Environment(\.presentationMode) var presentationMode
+
     @ObservedObject var item: BucKitItem
     @State var AddViewWithNavBarIsShown = false
+    
     var body: some View {
+        NavigationView {
             ScrollView(.vertical, showsIndicators: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/) {
                 GeometryReader { geometry in
                     VStack(alignment: .center ,spacing: 25) {
@@ -48,14 +51,76 @@ struct BucketItemDetailView: View {
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
-                .navigationBarItems(trailing: Button(action: {
+                .navigationBarItems(leading: Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    Text("Back")
+                }) ,trailing: Button(action: {
                     self.AddViewWithNavBarIsShown.toggle()
                 }) {
                     Text("Edit")
-                }.fullScreenCover(isPresented: $AddViewWithNavBarIsShown) {
+                }
+                .onAppear { item.loadPlaceMark() }
+                .fullScreenCover(isPresented: $AddViewWithNavBarIsShown) {
                     AddViewWithNavigationBar(buckitItem: item)
                 })
-                .onAppear { item.loadPlaceMark() }
+            }
+        }
+    }
+    
+    func arrayOfActivities() -> [Activity] {
+        return item.activities.map({$0})
+    }
+}
+
+struct BucketItemDetailView: View {
+    
+    @ObservedObject var item: BucKitItem
+    @State var AddViewWithNavBarIsShown = false
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/) {
+            GeometryReader { geometry in
+                VStack(alignment: .center ,spacing: 25) {
+                    CircleImage(width: geometry.size.width * 0.45, imageData: item.image)
+                        .padding()
+                    VStack {
+                        Text("Name: \(item.name)")
+                            .padding()
+                        Text("Date: \(item.stringDate())")
+                            .padding()
+                        
+                        Text("Location: \(item.location)")
+                    }
+                    VStack(alignment: .center) {
+                        Text("Activities")
+                            .font(.title2)
+                            .padding()
+                        
+                    }
+                    
+                    VStack (alignment: .leading) {
+                        ForEach(arrayOfActivities()) {activity in
+                            HStack {
+                                Image(systemName: "circle.fill")
+                                Text(activity.name)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                        }
+                    }
+                    NavigationLink("Travel Planner", destination: TravelPlanner(item: item))
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .navigationBarItems(trailing: Button(action: {
+                self.AddViewWithNavBarIsShown.toggle()
+            }) {
+                Text("Edit")
+            }.fullScreenCover(isPresented: $AddViewWithNavBarIsShown) {
+                AddViewWithNavigationBar(buckitItem: item)
+            })
+            .onAppear { item.loadPlaceMark() }
         }
     }
     
